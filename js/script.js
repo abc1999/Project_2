674e92a0baa1ceff90dd5ad5436628ab905233cd
abc1999 <pospelov@mail.ru>
2020-04-24 19:13:48 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// ПЕРЕМЕННЫЕ
+var message; // a-z, A-Z, 0-9, $, _
+var Message; // Имя переменной чувствительно к регистру
+var myMessage;
+myMessage = "Сообщение"; //запись в 2 строки: объявляем переменную, присваиваем значение
+
+var myNumber = 10; //запись в одну строку: сразу присваиваем значение
+var myString = "Привет, Дарт!";
+var myBoolean = true; // fals
+var myNull = null;
+var myUndefined = undefined;
+var myObject = {
+	name: "Дарт",
+	age: 45
+};
+
+// ЧИСЛА
+// var result = 40 + 10;
+// alert(result); // или
+// alert(40 + 10);
+console.log(40 + myNumber);
+console.log(40 - myNumber);
+console.log(40 * myNumber);
+console.log(40 / myNumber);
+myNumber = myNumber + 10; // иначе: += 10 или увеличение на 1 myNumber++, уменьшение на 1 myNumber--
+console.log(myNumber);
+
+
